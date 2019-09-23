9bf4583823d59869032d849156533e76455152fa
LevitskyAlexei <ydav-07@mail.ru>
2019-09-23 19:34:24 +0300

    mission completed)))))!!!!!!!

--- src/ru/forwork/first/DeleteFile.java
+++ src/ru/forwork/first/DeleteFile.java
@@ -1,36 +1,47 @@
 package ru.forwork.first;
 
 import java.io.File;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.io.FilenameFilter;
+import java.util.*;
 
 public class DeleteFile {
     public static void main(String[] args) {
-        createDirectory();
-        final String PATH = "D:/testjava";
+        //createDirectory();
+        final String PATH = "D:/BackUp/MSPlus-18";
         File dir = new File(PATH);
+
         File[] listOfFile = dir.listFiles();
-       // Collections.sort(listOfFile);
+        //sort dir on time_to_create
+        Arrays.sort(listOfFile, new Comparator<File>() {
+            @Override
+            public int compare(File o1, File o2) {
+                return Long.valueOf(o1.lastModified()).compareTo(o2.lastModified());
+            }
+        });
+
         for (int i = 0; i < listOfFile.length; i++){
             System.out.println(listOfFile[i]);
         }
-//        for (int i = 0; i < 30; i++){
-//            System.out.println(listOfFile[i].toString());
-//            recursiveDelete(listOfFile[i]);
-//        }
+        //delete 30
+        for (int i = 0; i < 30; i++){
+            System.out.println(listOfFile[i].toString());
+            recursiveDelete(listOfFile[i]);
+        }
 
     }
-    public static void createDirectory(){
-        final String PATH = "D:/testjava";
-        List list = new ArrayList();
-        for (int i = 0; i < 40; i++){
-            list.add(i);
-        }
-        for (int i = 0; i < list.size(); i++){
-            new File(PATH+"/"+list.get(i)).mkdir();
-        }
-    }
+    //create 40 dir for test
+//    public static void createDirectory(){
+//        final String PATH = "D:/INSTALL/BackUp/MSPlus-18";
+//        List list = new ArrayList();
+//        for (int i = 0; i < 40; i++){
+//            list.add(i);
+//        }
+//        for (int i = 0; i < list.size(); i++){
+//            new File(PATH+"/"+list.get(i)).mkdir();
+//        }
+//    }
+
+    //recursive delete
     public static void recursiveDelete(File file) {
         // до конца рекурсивного цикла
         if (!file.exists())
